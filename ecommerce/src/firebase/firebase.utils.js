8832ad736dc1b95d60c0010bdcc3e285a4133780
Santosh Kumar <santoshku8287@gmail.com>
2020-04-13 17:48:12 +0530

    added user authentication and signup

--- ecommerce/src/firebase/firebase.utils.js
+++ ecommerce/src/firebase/firebase.utils.js
@@ -12,6 +12,21 @@ const config = {
   appId: "1:531373771094:web:b38dee13159f9f35b22ef7"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
+};
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
