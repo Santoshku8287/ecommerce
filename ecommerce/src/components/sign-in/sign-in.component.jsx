769c842c141f79ab64f0ec5d4bceca9fdf7bb6f2
Santosh Kumar <santoshku8287@gmail.com>
2020-04-13 17:53:54 +0530

    sign-in component path issue fixed

--- ecommerce/src/components/sign-in/sign-in.component.jsx
+++ ecommerce/src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
-import CustomButton from "../../pages/custom-button/custom-button.component";
+import CustomButton from "../custom-button/custom-button.component";
 import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
